cd3232d54979a964052c50d4d07a0bd190c43f2e
tra007 <a.alirezaloo11@gmail.com>
2023-07-03 17:03:33 +0330

    feat (shop/admin): admin panel add some features

--- ben10/shop/admin.py
+++ ben10/shop/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Product
 
+
 # Register your models here.
 
-admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'owner',
+                    'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'available']
+    search_fields = ('name', 'id')
+
+    def get_prepopulated_fields(self, request, obj=None):
+        return {'slug': ('name',)}
