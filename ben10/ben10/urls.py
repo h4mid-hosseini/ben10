28720d5e84578d814862fadd5ccf8b5183c3d55d
tra007 <a.alirezaloo11@gmail.com>
2023-07-04 16:48:17 +0330

    feat (JWT and shop/products): implementation JWT and updated shop for products view and serializers

--- ben10/ben10/urls.py
+++ ben10/ben10/urls.py
@@ -11,14 +11,18 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    1. Import the include() function: from django.urls.py import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls.py'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
+                  path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+                  path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+                  path('api/shop/', include("shop.urls")),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- ben10/shop/views.py
+++ ben10/shop/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework.parsers import JSONParser
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from .serializers import ProductSerializers
+from .models import Product
+
 
 # Create your views here.
+
+class ProductViews(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        product = Product.objects.filter(owner__id=user.id)
+        serializer = ProductSerializers(product, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        data = JSONParser().parse(request)
+        serializer = ProductSerializers(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_201_CREATED)
+
+    def patch(self, request, product_id):
+        user = request.user
+        product = get_object_or_404(Product, id=product_id, owner__id=user.id)
+        data = JSONParser().parse(request)
+        serializer = ProductSerializers(data=data, instance=product, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    def delete(self, request, product_id):
+        user = request.user
+        product = get_object_or_404(Product, id=product_id, owner__id=user.id)
+        product.delete()
+        return Response(status.HTTP_204_NO_CONTENT)
